6d8330e044904ca1fae0a6b8380ba8df70d21004
Guillaume Manchon <guillaumemanchon@minideguillaume.home>
2023-12-11 19:56:44 +0100

    suppression

--- app/history/page.jsx
+++ app/history/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link";
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth"
-import { collection, getDocs, getDoc } from 'firebase/firestore'
+import { collection, getDocs, getDoc, deleteDoc, doc } from 'firebase/firestore'
 import { db, auth } from "@/firebase/firebase"
 import { useState, useEffect, useContext } from "react";
 
@@ -10,6 +10,7 @@ import { useThemeContext } from "@/context/context.jsx"
 export default function index() {
 
   const { user, setUser } = useThemeContext()
+  const [ isDelete, setIsDelete ] = useState(false)
   const [data, setData] = useState([]);
 
   //recuperation des données sur la DB FIRESTORE
@@ -31,7 +32,20 @@ export default function index() {
       }
     }
     getDataList()
-  }, [])
+  }, [isDelete])
+
+  const handleDelete = async (e) => {
+    e.preventDefault;
+    const idDelete = e.target.id
+    const isConfirmed = window.confirm('Etes-vous sûr ?')
+    if(isConfirmed){
+      await deleteDoc(doc(db, "news", idDelete))
+      // console.log('SUPPRIME');
+    }else{
+      return
+    }
+    setIsDelete(!isDelete)
+  }
 
   return (
     <>
@@ -39,14 +53,17 @@ export default function index() {
         <main className="flex flex-col stats shadow">
           {data.map((item) => {
             return (
-              <div key={item.id} className="stat btn-outline btn-info">
+              <div key={item.id} className="stat">
                 <div className="stat-title">{item.ville}</div>
                 <div className="flex justify-between">
                   <div className="stat-desc">{item.date}</div>
                   <div className="stat-desc">{item.heure}</div>
                 </div>
                 <div className="stat-value">Piétons :{item.pietons}</div>
-                <div className="stat-value">Voitures :{item.voitures}</div>
+                <div className="flex justify-between">
+                  <div className="stat-value">Voitures :{item.voitures}</div>
+                  <button id={item.id} onClick={handleDelete} className="btn btn-outline btn-error">X</button>
+                </div>
               </div>
             )
           })}
